313b4b803654b33048520a12a678e9cd2e0da785
Chin-Yun Yu <cy006@whitby.eecs.qmul.ac.uk>
2022-10-16 18:02:56 +0100

    fix: arguments error

--- models/ar_decoder.py
+++ models/ar_decoder.py
@@ -32,7 +32,7 @@ class Transformer(nn.Transformer):
         decoder = Decoder(num_decoder_layers, emb_dim,
                           nhead, head_dim,  **kwargs)
         super().__init__(d_model=emb_dim, custom_decoder=decoder,
-                         custom_encoder=encoder, batch_first=True,  **kwargs)
+                         custom_encoder=encoder, batch_first=True, **kwargs)
 
 
 class MIDI2SpecAR(nn.Module):
@@ -43,11 +43,11 @@ class MIDI2SpecAR(nn.Module):
         super().__init__()
         self.emb = nn.Embedding(num_emb, emb_dim)
         self.register_buffer('in_pos_emb', sinusoidal(
-            (max_input_length, emb_dim)))
+            shape=(max_input_length, emb_dim)))
         self.register_buffer('out_pos_emb', sinusoidal(
-            (max_output_length, emb_dim)))
+            shape=(max_output_length, emb_dim)))
         self.transformer = Transformer(
-            emb_dim, num_encoder_layers, num_decoder_layers, batch_first=True, **kwargs)
+            emb_dim, nhead, head_dim,  num_encoder_layers, num_decoder_layers, **kwargs)
         self.linear_in = nn.Linear(output_dim, emb_dim)
         self.linear_out = nn.Linear(emb_dim, output_dim)
 
