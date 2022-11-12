b97361ff307921fd996422ed1b1a56706bd53b12
Chin-Yun Yu <cy006@whitby.eecs.qmul.ac.uk>
2022-11-12 11:17:31 +0000

    feat: autoregressive inference for ar baseline

--- models/ar_decoder.py
+++ models/ar_decoder.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+from tqdm import tqdm
 
 from .encoder import geglu, Encoder, MultiheadAttention
 from .utils import sinusoidal
@@ -34,6 +35,16 @@ class Transformer(nn.Transformer):
         super().__init__(d_model=emb_dim, custom_decoder=decoder,
                          custom_encoder=encoder, batch_first=True, **kwargs)
 
+    def autoregressive_infer(self, tgt, src=None, memory=None,
+                             src_mask=None, tgt_mask=None, memory_mask=None,
+                             src_key_padding_mask=None, tgt_key_padding_mask=None, memory_key_padding_mask=None):
+        if memory is None:
+            memory = self.encoder(src, mask=src_mask,
+                                  src_key_padding_mask=src_key_padding_mask)
+        out = self.decoder(tgt, memory, tgt_mask=tgt_mask, memory_mask=memory_mask,
+                           tgt_key_padding_mask=tgt_key_padding_mask, memory_key_padding_mask=memory_key_padding_mask)
+        return out, memory
+
 
 class MIDI2SpecAR(nn.Module):
     def __init__(self,
@@ -62,3 +73,23 @@ class MIDI2SpecAR(nn.Module):
         x = self.transformer(midi, spec, tgt_mask=spec_tri_mask)
         x = self.linear_out(x)
         return x
+
+    def infer(self, midi_tokens, max_len=512, dither_amount=0.):
+        batch_size, seq_len = midi_tokens.shape
+        midi = self.emb(midi_tokens) + self.in_pos_emb[:seq_len]
+        spec = midi.new_zeros(
+            (batch_size, 1, self.linear_out.weight.shape[0]))
+        memory = None
+
+        for i in tqdm(range(max_len)):
+            spec_emb = self.linear_in(spec) + self.out_pos_emb[:i+1]
+            next_spec, memory = self.transformer.autoregressive_infer(
+                spec_emb, src=midi, memory=memory)
+            next_spec = self.linear_out(next_spec[:, -1:])
+            if dither_amount > 0:
+                next_spec = next_spec + dither_amount * \
+                    torch.randn_like(next_spec)
+                next_spec.clamp_(-1, 1)
+            spec = torch.cat([spec, next_spec], dim=1)
+
+        return spec[:, 1:]
