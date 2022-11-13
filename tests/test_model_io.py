6714641e2266f67742ecfa52744a44f0fdb9b327
Chin-Yun Yu <cy006@whitby.eecs.qmul.ac.uk>
2022-11-13 16:08:12 +0000

    test: midi2spec

--- tests/test_model_io.py
+++ tests/test_model_io.py
@@ -1,8 +1,8 @@
 import pytest
 import torch
 from models.encoder import Encoder
-from models.ar_decoder import Decoder, Transformer
-from models.diff_decoder import DiffDecoder, DiffTransformer
+from models.ar_decoder import Decoder, Transformer, MIDI2SpecAR
+from models.diff_decoder import DiffDecoder, DiffTransformer, MIDI2SpecDiff
 
 
 def test_encoder():
@@ -73,3 +73,24 @@ def test_diff_transformer():
     y = transformer(x, y, cond, context,
                     dropout_mask=torch.zeros_like(dropout_mask))
     assert y.shape == (10, 64, 512)
+
+
+def test_midi2spec_ar():
+    model = MIDI2SpecAR(900, 128, 256, 64,
+                        512, 6, 64, 6, 6, dim_feedforward=1024)
+
+    midi = torch.randint(0, 900, (10, 256))
+    spec = torch.randn(10, 64, 128)
+    y = model(midi, spec)
+    assert y.shape == (10, 64, 128)
+
+
+def test_midi2spec_diff():
+    model = MIDI2SpecDiff(900, 128, 256, 64,
+                          512, 6, 64, 6, 6, with_context=False, dim_feedforward=1024)
+
+    midi = torch.randint(0, 900, (10, 256))
+    spec = torch.randn(10, 64, 128)
+    t = torch.rand(10)
+    y = model(midi, spec, t)
+    assert y.shape == (10, 64, 128)
