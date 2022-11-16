f79e2f9d4d44c8a5fa0a5e48f2f827eebf486051
Chin-Yun Yu <cy006@whitby.eecs.qmul.ac.uk>
2022-11-16 12:50:26 +0000

    feat: let dataset decide on/off context

--- data/mock.py
+++ data/mock.py
@@ -10,23 +10,26 @@ class MockSpecDataset(Dataset):
                  time_bins: int = 256,
                  num_tokens: int = 2048,
                  num_classes: int = 900,
-                 num_chunks: int = 100):
+                 num_chunks: int = 100,
+                 with_context: bool = False):
 
         self.spec_data = torch.rand(num_chunks, time_bins, freq_bins) * 2 - 1
         self.midi_data = torch.randint(
             0, num_classes, (num_chunks, num_tokens))
+
+        self.with_context = with_context
 
     def __len__(self) -> int:
         return self.spec_data.shape[0]
 
     def __getitem__(self, idx) -> Tuple[Tensor, Tensor, Tensor]:
         spec = self.spec_data[idx]
-        if idx:
-            context = self.spec_data[idx - 1]
-        else:
-            context = torch.zeros_like(spec)
         midi = self.midi_data[idx]
-        return midi, spec, context
+        if idx and self.with_context:
+            return midi, spec, self.spec_data[idx - 1]
+        elif self.with_context:
+            return midi, spec, torch.zeros_like(spec)
+        return midi, spec
 
 
 class MockAudioDataset(Dataset):
@@ -34,20 +37,23 @@ class MockAudioDataset(Dataset):
                  num_chunks: int = 100,
                  chunk_size: int = 16000,
                  num_classes: int = 900,
-                 num_tokens: int = 2048):
+                 num_tokens: int = 2048,
+                 with_context: bool = False):
 
         self.audio_data = torch.rand(num_chunks, chunk_size) * 2 - 1
         self.midi_data = torch.randint(
             0, num_classes, (num_chunks, num_tokens))
+
+        self.with_context = with_context
 
     def __len__(self) -> int:
         return self.audio_data.shape[0]
 
     def __getitem__(self, idx) -> Tuple[Tensor, Tensor, Tensor]:
         audio = self.audio_data[idx]
-        if idx:
-            context = self.audio_data[idx - 1]
-        else:
-            context = torch.zeros_like(audio)
         midi = self.midi_data[idx]
-        return midi, audio, context
+        if idx and self.with_context:
+            return midi, audio, self.audio_data[idx - 1]
+        elif self.with_context:
+            return midi, audio, torch.zeros_like(audio)
+        return midi, audio
--- lightning/mock.py
+++ lightning/mock.py
@@ -4,20 +4,20 @@ from data.mock import MockSpecDataset, MockAudioDataset
 
 
 class MockData(pl.LightningDataModule):
-    def __init__(self, batch_size: int, use_wav: bool = False):
+    def __init__(self, batch_size: int, use_wav: bool = False, with_context: bool = False):
         super().__init__()
         self.save_hyperparameters()
 
     def setup(self, stage=None):
         if stage == "fit" or stage is None:
-            self.train_dataset = MockSpecDataset(
-            ) if not self.hparams.use_wav else MockAudioDataset()
-            self.val_dataset = MockSpecDataset(
-                num_chunks=10) if not self.hparams.use_wav else MockAudioDataset(num_chunks=10)
+            self.train_dataset = MockSpecDataset(with_context=self.hparams.with_context) if not self.hparams.use_wav else MockAudioDataset(
+                with_context=self.hparams.with_context)
+            self.val_dataset = MockSpecDataset(with_context=self.hparams.with_context) if not self.hparams.use_wav else MockAudioDataset(
+                with_context=self.hparams.with_context)
 
         if stage == "test":
-            self.test_dataset = MockSpecDataset(
-                num_chunks=10) if not self.hparams.use_wav else MockAudioDataset(num_chunks=10)
+            self.test_dataset = MockSpecDataset(with_context=self.hparams.with_context) if not self.hparams.use_wav else MockAudioDataset(
+                with_context=self.hparams.with_context)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.hparams.batch_size, shuffle=True, num_workers=1)
@@ -27,4 +27,3 @@ class MockData(pl.LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.hparams.batch_size, shuffle=False, num_workers=1)
-
