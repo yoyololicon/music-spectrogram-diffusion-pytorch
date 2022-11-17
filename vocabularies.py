e567a34a23a42e1d8dae694ee1827fa7bf6e2101
Ningzhi Wang <ningzhi.wang@qmul.ac.uk>
2022-11-17 18:49:05 +0000

    prototype for tokenisation and segmentation

--- vocabularies.py
+++ vocabularies.py
@@ -1,5 +1,21 @@
+import dataclasses
+import math
 import note_seq
-from .event_codec import EventRange, Codec
+from event_codec import EventRange, Codec
+
+
+# defaults for vocabulary config
+DEFAULT_STEPS_PER_SECOND = 100
+DEFAULT_MAX_SHIFT_SECONDS = 10
+DEFAULT_NUM_VELOCITY_BINS = 127
+
+
+@dataclasses.dataclass
+class VocabularyConfig:
+  """Vocabulary configuration parameters."""
+  steps_per_second: int = DEFAULT_STEPS_PER_SECOND
+  max_shift_seconds: int = DEFAULT_MAX_SHIFT_SECONDS
+  num_velocity_bins: int = DEFAULT_NUM_VELOCITY_BINS
 
 
 def build_codec(steps_per_second, max_shift_seconds, num_velocity_bins=1):
@@ -23,3 +39,23 @@ def build_codec(steps_per_second, max_shift_seconds, num_velocity_bins=1):
       max_shift_steps=(steps_per_second * max_shift_seconds),
       steps_per_second=steps_per_second,
       event_ranges=event_ranges)
+
+
+def num_velocity_bins_from_codec(codec: Codec):
+  """Get number of velocity bins from event codec."""
+  lo, hi = codec.event_type_range('velocity')
+  return hi - lo
+
+
+def velocity_to_bin(velocity, num_velocity_bins):
+  if velocity == 0:
+    return 0
+  else:
+    return math.ceil(num_velocity_bins * velocity / note_seq.MAX_MIDI_VELOCITY)
+
+
+def bin_to_velocity(velocity_bin, num_velocity_bins):
+  if velocity_bin == 0:
+    return 0
+  else:
+    return int(note_seq.MAX_MIDI_VELOCITY * velocity_bin / num_velocity_bins)
