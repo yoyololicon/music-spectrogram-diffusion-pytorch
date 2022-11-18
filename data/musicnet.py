d85d097c3ecc63fe9def56494b57115299aa527f
Chin-Yun Yu <cy006@wynne.eecs.qmul.ac.uk>
2022-11-18 17:07:04 +0000

    fix: add with_context

--- data/musicnet.py
+++ data/musicnet.py
@@ -19,6 +19,7 @@ class MusicNet(Dataset):
                  split: str = 'train',
                  sample_rate: int = None,
                  segment_length: int = 81920,
+                 with_context: bool = False,
                  **midi_kwargs):
         super().__init__()
 
@@ -66,42 +67,58 @@ class MusicNet(Dataset):
             # ...
             midi = None
             data_list.append((wav_file, midi, sr, chunks))
-        
+
         self.data_list = data_list
         self.boundaries = np.cumsum(np.array([0] + [x[3] for x in data_list]))
         self.sr = sample_rate
         self.segment_length = segment_length
-            
+        self.with_context = with_context
 
     def __getitem__(self, index: int) -> torch.Tensor:
         bin_pos = np.digitize(index, self.boundaries[1:], right=False)
         wav_file, midi, sr, chunks = self.data_list[bin_pos]
+        # tokens = midi[chunk_index]
+        tokens = np.random.randint(0, 10, 2048)
+
         if self.sr is None or sr == self.sr:
             chunk_frames = self.segment_length
         else:
             chunk_frames = int(self.segment_length * sr / self.sr)
-        
+
         chunk_index = index - self.boundaries[bin_pos]
         offset = chunk_index * chunk_frames
+
+        if not self.with_context:
+            data, _ = sf.read(
+                wav_file, start=offset, frames=chunk_frames, dtype='float32', always_2d=True)
+            data = data.mean(axis=1)
+            if self.sr is not None and sr != self.sr:
+                data = resampy.resample(data, sr, self.sr, axis=0, filter='kaiser_fast')[
+                    :self.segment_length]
+                if data.shape[0] < self.segment_length:
+                    data = np.pad(data, ((0, self.segment_length - data.shape[0]),), 'constant')
+            return tokens, data
+
         ctx_offset = offset - chunk_frames
-        if ctx_offset < 0:
-            data, _ = sf.read(wav_file, start=offset, frames=chunk_frames, dtype='float32', always_2d=True)
-            ctx = np.zeros_like(data)
-        else:
-            ctx, _ = sf.read(wav_file, start=ctx_offset, frames=chunk_frames * 2, dtype='float32', always_2d=True)
+        if ctx_offset >= 0:
+            ctx, _ = sf.read(wav_file, start=ctx_offset,
+                             frames=chunk_frames * 2, dtype='float32', always_2d=True)
             data = ctx[chunk_frames:]
             ctx = ctx[:chunk_frames]
+        else:
+            data, _ = sf.read(
+                wav_file, start=offset, frames=chunk_frames, dtype='float32', always_2d=True)
+            ctx = np.zeros_like(data)
         data = data.mean(axis=1)
         ctx = ctx.mean(axis=1)
         if self.sr is not None and sr != self.sr:
             tmp = np.vstack([ctx, data])
-            tmp = resampy.resample(tmp, sr, self.sr, axis=1, filter='kaiser_fast')[:, :self.segment_length]
+            tmp = resampy.resample(tmp, sr, self.sr, axis=1, filter='kaiser_fast')[
+                :, :self.segment_length]
             if tmp.shape[1] < self.segment_length:
-                tmp = np.pad(tmp, ((0, 0), (0, self.segment_length - tmp.shape[1])), 'constant')
+                tmp = np.pad(
+                    tmp, ((0, 0), (0, self.segment_length - tmp.shape[1])), 'constant')
             ctx, data = tmp
-
-        # tokens = midi[chunk_index]
-        tokens = np.random.randint(0, 10, 2048)
         return tokens, data, ctx
 
     def __len__(self) -> int:
