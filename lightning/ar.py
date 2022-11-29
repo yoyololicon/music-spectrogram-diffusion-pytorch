fix: validation loss calculation

lightning/ar.py
@@ -62,12 +62,11 @@ class AutoregressiveLM(pl.LightningModule):
         past_spec[:, 0] = 0
         pred = self.model(midi, past_spec)
         loss = F.mse_loss(pred, spec)
-
-        values = {
-            'val_loss': loss,
-        }
-        self.log_dict(values, prog_bar=True, sync_dist=True)
         return loss
+
+    def validation_epoch_end(self, outputs) -> None:
+        avg_loss = sum(outputs) / len(outputs)
+        self.log('val_loss', avg_loss, prog_bar=True, sync_dist=True)
 
     def configure_optimizers(self):
         return optim.Adafactor(self.parameters(), lr=1e-3)