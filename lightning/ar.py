0a176e2069ffb94b4011944341bb66e77d65a667
Chin-Yun Yu <cy006@bluck.eecs.qmul.ac.uk>
2022-12-13 17:11:39 +0000

    fix: disable inner tqdm when testing

--- lightning/ar.py
+++ lightning/ar.py
@@ -86,7 +86,7 @@ class AutoregressiveLM(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         midi, orig_wav, *_ = batch
         spec = self.mel(orig_wav)
-        pred = self.model.infer(midi)
+        pred = self.model.infer(midi, verbose=False)
         loss = F.mse_loss(pred, spec)
         pred_wav = self.spec_to_wav(pred)
         metric = calculate_metrics(
