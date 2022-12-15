36db86257abf4ee304538486cdf50f777f1ae426
Chin-Yun Yu <cy006@bluck.eecs.qmul.ac.uk>
2022-12-15 10:55:16 +0000

    feat: disable finding unused params for ddp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 from pytorch_lightning.cli import LightningCLI
 from pytorch_lightning.callbacks import ModelCheckpoint, ModelSummary
+from pytorch_lightning.strategies import DDPStrategy
 
 from lightning import AutoregressiveLM, DiffusionLM
 from lightning.mock import MockData
@@ -11,7 +12,7 @@ def cli_main():
     cli = LightningCLI(
         trainer_defaults={
             'accelerator': 'gpu',
-            'strategy': 'ddp',
+            'strategy': DDPStrategy(find_unused_parameters=False),
             'log_every_n_steps': 1,
             'callbacks': [
                 ModelCheckpoint(
